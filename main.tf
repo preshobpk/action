545255f165bcea04557d9d26c6b000daea2b8ae0
Preshob <preshobpk@gmail.com>
2023-07-16 12:13:46 +0000

    added SG dev

---
+++ main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+variable "proj_name" {
+  default = "zomato"
+}
+variable "proj_env" {
+  default = "prod"
+}
+resource "aws_security_group" "zomato-sg" {
+  name        = "${var.proj_name}-${var.proj_env}-SG"
+  description = "zomato-sg"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  tags = {
+    Name    = "${var.proj_name}-${var.proj_env}-SG"
+    project = "var.proj_name"
+    env     = "var.proj_env"
+  }
+}
